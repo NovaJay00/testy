d6d7f8bd4bb9e03c5535cee68f584f19513135b4
NovaJay00 <jpawar010@gmail.com>
2022-03-10 12:27:28 +0530

    Updates

---
+++ precache-manifest.185f17696e428ea1f1d0569e38f1a442.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d80f4733e92f5cb9b052456641dec0",
+    "url": "/testy/index.html"
+  },
+  {
+    "revision": "8f42743c162a9a76fc87",
+    "url": "/testy/static/css/2.aa9bd187.chunk.css"
+  },
+  {
+    "revision": "fbc72c2458dbd58982de",
+    "url": "/testy/static/css/main.7877a41f.chunk.css"
+  },
+  {
+    "revision": "8f42743c162a9a76fc87",
+    "url": "/testy/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "fbc72c2458dbd58982de",
+    "url": "/testy/static/js/main.767afc3f.chunk.js"
+  },
+  {
+    "revision": "65b370b12dd7e317a5b9",
+    "url": "/testy/static/js/runtime~main.01250910.js"
+  },
+  {
+    "revision": "55d5567d5080cf9a8f294bad872ffc08",
+    "url": "/testy/static/media/blur-background-1187974.55d5567d.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/testy/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/testy/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/testy/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/testy/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/testy/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
